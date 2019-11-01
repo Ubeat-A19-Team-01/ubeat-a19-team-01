9647d90d97cbf43c332ddcf38d091621174903ed
Raoul N'guessan <rnguess3@gmail.com>
2019-10-31 21:23:08 -0400

    add class Albums

---
+++ src/api/Albums.js
@@ -0,0 +1,18 @@
+class Albums {
+
+    getAlbumsById = async(url, id) => {
+        const response = await fetch(url + `/albums/${id}`, {
+            method: 'GET'
+        });
+        return await response.json();
+    };
+
+    getTracksByAlbumsById = async(url, id) => {
+        const response = await fetch(url + `/albums/${id}/tracks`, {
+            method: 'GET'
+        });
+        return await response.json();
+    }
+}
+
+export default Albums;
