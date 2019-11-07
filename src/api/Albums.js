ad647c777f9babf537adfe2b101de052386f9460
Raoul N'guessan <rnguess3@gmail.com>
2019-11-06 23:10:31 -0500

    modification de la vue album

--- src/api/Albums.js
+++ src/api/Albums.js
@@ -1,13 +1,13 @@
 /* Module Albums */
 
-    getAlbumsById = async(url, id) => {
+ export const getAlbumsById = async(url, id) => {
         const response = await fetch(url + `/albums/${id}`, {
             method: 'GET'
         });
         return await response.json();
-    };
+    }
 
-    getTracksByAlbumsById = async(url, id) => {
+ export const getTracksByAlbumsById = async(url, id) => {
         const response = await fetch(url + `/albums/${id}/tracks`, {
             method: 'GET'
         });
--- src/components/common/Home.vue
+++ src/components/common/Home.vue
@@ -1,10 +1,28 @@
 <template>
-    <h2>I am home here</h2>
+<v-container fluid>
+    <v-layout wrap row justify-space-between>
+    <v-flex>
+  <Album :AlbumId=Id  ></Album>
+  </v-flex>
+    </v-layout>
+</v-container>
+  
+  
 </template>
 
 <script>
+import Album from '../albums/Album.vue'
     export default {
-        name: "Home"
+        name: "Home",
+        components:{
+            Album
+        } , 
+         data :() =>(
+            {
+              Id:'929463779'
+            }
+
+        )
     }
 </script>
 
